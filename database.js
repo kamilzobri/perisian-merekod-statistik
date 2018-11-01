2ffecf160145b2c1fcfd6596fd4031e9c6f61059
kamil <kamilzobri@kamils-MacBook-Air.local>
2018-11-01 14:49:29 +0800

    Initial commit

---
+++ database.js
@@ -0,0 +1,353 @@
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+const adapter = new FileSync('db.json')
+const db = low(adapter)
+
+
+var today = new Date();
+var hari = today.getDate();
+var bulan = today.getMonth() + 1; //January is 0!
+var tahun = today.getFullYear();
+
+today = hari + '.' + bulan + '.' + tahun;
+
+
+
+if (!db.has(`${tahun}`).value()) {
+  db.set(`${tahun}`, {}).write()
+}
+if (!db.has(`${tahun}.${bulan}`).value()) {
+  db.set(`${tahun}.${bulan}`, {}).write()
+}
+if (!db.has(`${tahun}.${bulan}.${hari}`).value()) {
+  db.set(`${tahun}.${bulan}.${hari}`, {}).write()
+  db.set(`${tahun}.${bulan}.${hari}.belia.pengunjung.melayu`, 0).write()
+  db.set(`${tahun}.${bulan}.${hari}.dewasa.pengunjung.melayu`, 0).write()
+  db.set(`${tahun}.${bulan}.${hari}.belia.pengunjung.cina`, 0).write()
+  db.set(`${tahun}.${bulan}.${hari}.dewasa.pengunjung.cina`, 0).write()
+  db.set(`${tahun}.${bulan}.${hari}.belia.pengunjung.india`, 0).write()
+  db.set(`${tahun}.${bulan}.${hari}.dewasa.pengunjung.india`, 0).write()
+  db.set(`${tahun}.${bulan}.${hari}.belia.pengunjung.lainlain`, 0).write()
+  db.set(`${tahun}.${bulan}.${hari}.dewasa.pengunjung.lainlain`, 0).write()
+
+  db.set(`${tahun}.${bulan}.${hari}.belia.pengguna.melayu`, 0).write()
+  db.set(`${tahun}.${bulan}.${hari}.dewasa.pengguna.melayu`, 0).write()
+  db.set(`${tahun}.${bulan}.${hari}.belia.pengguna.cina`, 0).write()
+  db.set(`${tahun}.${bulan}.${hari}.dewasa.pengguna.cina`, 0).write()
+  db.set(`${tahun}.${bulan}.${hari}.belia.pengguna.india`, 0).write()
+  db.set(`${tahun}.${bulan}.${hari}.dewasa.pengguna.india`, 0).write()
+  db.set(`${tahun}.${bulan}.${hari}.belia.pengguna.lainlain`, 0).write()
+  db.set(`${tahun}.${bulan}.${hari}.dewasa.pengguna.lainlain`, 0).write()
+
+  db.set(`${tahun}.${bulan}.${hari}.belia.ahliBaru.melayu`, 0).write()
+  db.set(`${tahun}.${bulan}.${hari}.dewasa.ahliBaru.melayu`, 0).write()
+  db.set(`${tahun}.${bulan}.${hari}.belia.ahliBaru.cina`, 0).write()
+  db.set(`${tahun}.${bulan}.${hari}.dewasa.ahliBaru.cina`, 0).write()
+  db.set(`${tahun}.${bulan}.${hari}.belia.ahliBaru.india`, 0).write()
+  db.set(`${tahun}.${bulan}.${hari}.dewasa.ahliBaru.india`, 0).write()
+  db.set(`${tahun}.${bulan}.${hari}.belia.ahliBaru.lainlain`, 0).write()
+  db.set(`${tahun}.${bulan}.${hari}.dewasa.ahliBaru.lainlain`, 0).write()
+}
+
+function updateData(userType, userRace, usia) {
+  jumlahTambah = +document.getElementById('bilNakUpdate').value;
+  this.userType = userType;
+  this.userRace = userRace;
+  this.usia = usia;
+  var usiaUppercase = usia.charAt(0).toUpperCase() + usia.slice(1);
+  var userTypeUppercase = userType.charAt(0).toUpperCase() + userType.slice(1);
+  db.update(`${tahun}.${bulan}.${hari}.${usia}.${userType}.${userRace}`, n => n + jumlahTambah)
+    .write()
+
+  if (userType == 'pengguna') {
+    db.update(`${tahun}.${bulan}.${hari}.${usia}.pengunjung.${userRace}`, n => n + jumlahTambah)
+      .write()
+    document.getElementById(`${userRace + usiaUppercase}PengunjungSemasa`).value = db.get(`${tahun}.${bulan}.${hari}.${usia}.${userType}.${userRace}`).value();
+  }
+  document.getElementById("bilNakUpdate").value = 1;
+  document.getElementById("userType").selectedIndex = 0;
+  lastUpdate(`${userType} ${userRace} ${usia} +${jumlahTambah}`)
+
+  document.getElementById(`${userRace + usiaUppercase + userTypeUppercase}Semasa`).value = db.get(`${tahun}.${bulan}.${hari}.${usia}.${userType}.${userRace}`).value();
+}
+
+//function update belia
+function updateCinaBeliaPengguna() {
+  var cinaBelia = +document.getElementById("cinaBeliaPengguna").value;
+  db.update(`${tahun}.${bulan}.${hari}.belia.pengguna.cina`, n => n + cinaBelia)
+    .write()
+  db.update(`${tahun}.${bulan}.${hari}.belia.pengunjung.cina`, n => n + cinaBelia)
+    .write()
+  document.getElementById("cinaBeliaPengguna").value = 1;
+  document.getElementById("cinaBeliaPenggunaSemasa").value = db.get(`${tahun}.${bulan}.${hari}.belia.pengguna.cina`).value();
+  document.getElementById("cinaBeliaPengunjungSemasa").value = db.get(`${tahun}.${bulan}.${hari}.belia.pengunjung.cina`).value();
+  lastUpdate(`Pengguna Cina Belia + ${cinaBelia}`)
+}
+function updateCinaBeliaPengunjung() {
+  var cinaBelia = +document.getElementById("cinaBeliaPengunjung").value;
+  db.update(`${tahun}.${bulan}.${hari}.belia.pengunjung.cina`, n => n + cinaBelia)
+    .write()
+  document.getElementById("cinaBeliaPengunjung").value = 1;
+  document.getElementById("cinaBeliaPengunjungSemasa").value = db.get(`${tahun}.${bulan}.${hari}.belia.pengunjung.cina`).value();
+
+  lastUpdate(`Pengunjung Cina Belia + ${cinaBelia}`)
+
+}
+function updateCinaBeliaAhliBaru() {
+  var cinaBelia = +document.getElementById("cinaBeliaAhliBaru").value;
+  db.update(`${tahun}.${bulan}.${hari}.belia.ahliBaru.cina`, n => n + cinaBelia)
+    .write()
+  document.getElementById("cinaBeliaAhliBaru").value = 1;
+  document.getElementById("cinaBeliaAhliBaruSemasa").value = db.get(`${tahun}.${bulan}.${hari}.belia.ahliBaru.cina`).value();
+
+  lastUpdate(`Ahli Baru Cina Belia + ${cinaBelia}`)
+}
+
+function updateMelayuBeliaPengguna() {
+  var melayuBelia = +document.getElementById("melayuBeliaPengguna").value;
+  db.update(`${tahun}.${bulan}.${hari}.belia.pengguna.melayu`, n => n + melayuBelia)
+    .write()
+  db.update(`${tahun}.${bulan}.${hari}.belia.pengunjung.melayu`, n => n + melayuBelia)
+    .write()
+  document.getElementById("melayuBeliaPengguna").value = 1;
+  document.getElementById("melayuBeliaPenggunaSemasa").value = db.get(`${tahun}.${bulan}.${hari}.belia.pengguna.melayu`).value();
+  document.getElementById("melayuBeliaPengunjungSemasa").value = db.get(`${tahun}.${bulan}.${hari}.belia.pengunjung.melayu`).value();
+
+  lastUpdate(`Pengguna Melayu Belia + ${melayuBelia}`)
+
+}
+function updateMelayuBeliaPengunjung() {
+  var melayuBelia = +document.getElementById("melayuBeliaPengunjung").value;
+  db.update(`${tahun}.${bulan}.${hari}.belia.pengunjung.melayu`, n => n + melayuBelia)
+    .write()
+  document.getElementById("melayuBeliaPengunjung").value = 1;
+  document.getElementById("melayuBeliaPengunjungSemasa").value = db.get(`${tahun}.${bulan}.${hari}.belia.pengunjung.melayu`).value();
+
+  lastUpdate(`Pengunjung Melayu Belia + ${melayuBelia}`)
+}
+function updateMelayuBeliaAhliBaru() {
+  var melayuBelia = +document.getElementById("melayuBeliaAhliBaru").value;
+  db.update(`${tahun}.${bulan}.${hari}.belia.ahliBaru.melayu`, n => n + melayuBelia)
+    .write()
+  document.getElementById("melayuBeliaAhliBaru").value = 1;
+  document.getElementById("melayuBeliaAhliBaruSemasa").value = db.get(`${tahun}.${bulan}.${hari}.belia.ahliBaru.melayu`).value();
+
+  lastUpdate(`Ahli Baru Melayu Belia + ${melayuBelia}`)
+}
+
+
+function updateIndiaBeliaPengguna() {
+  var indiaBelia = +document.getElementById("indiaBeliaPengguna").value;
+  db.update(`${tahun}.${bulan}.${hari}.belia.pengguna.india`, n => n + indiaBelia)
+    .write()
+  db.update(`${tahun}.${bulan}.${hari}.belia.pengunjung.india`, n => n + indiaBelia)
+    .write()
+  document.getElementById("indiaBeliaPengguna").value = 1;
+  document.getElementById("indiaBeliaPenggunaSemasa").value = db.get(`${tahun}.${bulan}.${hari}.belia.pengguna.india`).value();
+  document.getElementById("indiaBeliaPengunjungSemasa").value = db.get(`${tahun}.${bulan}.${hari}.belia.pengunjung.india`).value();
+
+  lastUpdate(`Pengguna India Belia + ${indiaBelia}`)
+}
+function updateIndiaBeliaPengunjung() {
+  var indiaBelia = +document.getElementById("indiaBeliaPengunjung").value;
+  db.update(`${tahun}.${bulan}.${hari}.belia.pengunjung.india`, n => n + indiaBelia)
+    .write()
+  document.getElementById("indiaBeliaPengunjung").value = 1;
+  document.getElementById("indiaBeliaPengunjungSemasa").value = db.get(`${tahun}.${bulan}.${hari}.belia.pengunjung.india`).value();
+
+  lastUpdate(`Pengunjung India Belia + ${indiaBelia}`)
+
+}
+function updateIndiaBeliaAhliBaru() {
+  var indiaBelia = +document.getElementById("indiaBeliaAhliBaru").value;
+  db.update(`${tahun}.${bulan}.${hari}.belia.ahliBaru.india`, n => n + indiaBelia)
+    .write()
+  document.getElementById("indiaBeliaAhliBaru").value = 1;
+  document.getElementById("indiaBeliaAhliBaruSemasa").value = db.get(`${tahun}.${bulan}.${hari}.belia.ahliBaru.india`).value();
+
+  lastUpdate(`Ahli Baru India Belia + ${indiaBelia}`)
+}
+
+
+function updateLainLainBeliaPengguna() {
+  var lainBelia = +document.getElementById("lainlainBeliaPengguna").value;
+  db.update(`${tahun}.${bulan}.${hari}.belia.pengguna.lainlain`, n => n + lainBelia)
+    .write()
+  db.update(`${tahun}.${bulan}.${hari}.belia.pengunjung.lainlain`, n => n + lainBelia)
+    .write()
+  document.getElementById("lainlainBeliaPengguna").value = 1;
+  document.getElementById("lainlainBeliaPenggunaSemasa").value = db.get(`${tahun}.${bulan}.${hari}.belia.pengguna.lainlain`).value();
+  document.getElementById("lainlainBeliaPengunjungSemasa").value = db.get(`${tahun}.${bulan}.${hari}.belia.pengunjung.lainlain`).value();
+  lastUpdate(`Pengguna Lain-lain Belia + ${lainBelia}`)
+
+}
+function updateLainLainBeliaPengunjung() {
+  var lainBelia = +document.getElementById("lainlainBeliaPengunjung").value;
+  db.update(`${tahun}.${bulan}.${hari}.belia.pengunjung.lainlain`, n => n + lainBelia)
+    .write()
+  document.getElementById("lainlainBeliaPengunjung").value = 1;
+  document.getElementById("lainlainBeliaPengunjungSemasa").value = db.get(`${tahun}.${bulan}.${hari}.belia.pengunjung.lainlain`).value();
+
+  lastUpdate(`Pengunjung Lain-lain Belia + ${lainBelia}`)
+
+}
+function updateLainLainBeliaAhliBaru() {
+  var lainBelia = +document.getElementById("lainlainBeliaAhliBaru").value;
+  db.update(`${tahun}.${bulan}.${hari}.belia.ahliBaru.lainlain`, n => n + lainBelia)
+    .write()
+  document.getElementById("lainlainBeliaAhliBaru").value = 1;
+  document.getElementById("lainlainBeliaAhliBaruSemasa").value = db.get(`${tahun}.${bulan}.${hari}.belia.ahliBaru.lainlain`).value();
+
+  lastUpdate(`Ahli Baru Lain-lain Belia + ${lainBelia}`)
+}
+
+
+
+
+
+//function update dewasa
+
+function updateMelayuDewasaPengguna() {
+  var melayuDewasa = +document.getElementById("melayuDewasaPengguna").value;
+  db.update(`${tahun}.${bulan}.${hari}.dewasa.pengguna.melayu`, n => n + melayuDewasa)
+    .write()
+  db.update(`${tahun}.${bulan}.${hari}.dewasa.pengunjung.melayu`, n => n + melayuDewasa)
+    .write()
+  document.getElementById("melayuDewasaPengguna").value = 1;
+  document.getElementById("melayuDewasaPenggunaSemasa").value = db.get(`${tahun}.${bulan}.${hari}.dewasa.pengguna.melayu`).value();
+  document.getElementById("melayuDewasaPengunjungSemasa").value = db.get(`${tahun}.${bulan}.${hari}.dewasa.pengunjung.melayu`).value();
+
+  lastUpdate(`Pengguna Melayu Dewasa + ${melayuDewasa}`)
+
+}
+function updateMelayuDewasaPengunjung() {
+  var melayuDewasa = +document.getElementById("melayuDewasaPengunjung").value;
+  db.update(`${tahun}.${bulan}.${hari}.dewasa.pengunjung.melayu`, n => n + melayuDewasa)
+    .write()
+  document.getElementById("melayuDewasaPengunjung").value = 1;
+  document.getElementById("melayuDewasaPengunjungSemasa").value = db.get(`${tahun}.${bulan}.${hari}.dewasa.pengunjung.melayu`).value();
+
+  lastUpdate(`Pengunjung Melayu Dewasa + ${melayuDewasa}`)
+}
+function updateMelayuDewasaAhliBaru() {
+  var melayuDewasa = +document.getElementById("melayuDewasaAhliBaru").value;
+  db.update(`${tahun}.${bulan}.${hari}.dewasa.ahliBaru.melayu`, n => n + melayuDewasa)
+    .write()
+  document.getElementById("melayuDewasaAhliBaru").value = 1;
+  document.getElementById("melayuDewasaAhliBaruSemasa").value = db.get(`${tahun}.${bulan}.${hari}.dewasa.ahliBaru.melayu`).value();
+
+  lastUpdate(`Ahli Baru Melayu Dewasa + ${melayuDewasa}`)
+}
+
+
+function updateCinaDewasaPengguna() {
+  var cinaDewasa = +document.getElementById("cinaDewasaPengguna").value;
+  db.update(`${tahun}.${bulan}.${hari}.dewasa.pengguna.cina`, n => n + cinaDewasa)
+    .write()
+  db.update(`${tahun}.${bulan}.${hari}.dewasa.pengunjung.cina`, n => n + cinaDewasa)
+    .write()
+  document.getElementById("cinaDewasaPengguna").value = 1;
+  document.getElementById("cinaDewasaPenggunaSemasa").value = db.get(`${tahun}.${bulan}.${hari}.dewasa.pengguna.cina`).value();
+  document.getElementById("cinaDewasaPengunjungSemasa").value = db.get(`${tahun}.${bulan}.${hari}.dewasa.pengunjung.cina`).value();
+
+  lastUpdate(`Pengguna Cina Dewasa + ${cinaDewasa}`)
+}
+function updateCinaDewasaPengunjung() {
+  var cinaDewasa = +document.getElementById("cinaDewasaPengunjung").value;
+  db.update(`${tahun}.${bulan}.${hari}.dewasa.pengunjung.cina`, n => n + cinaDewasa)
+    .write()
+  document.getElementById("cinaDewasaPengunjung").value = 1;
+  document.getElementById("cinaDewasaPengunjungSemasa").value = db.get(`${tahun}.${bulan}.${hari}.dewasa.pengunjung.cina`).value();
+
+  lastUpdate(`Pengunjung Cina Dewasa + ${cinaDewasa}`)
+}
+function updateCinaDewasaAhliBaru() {
+  var cinaDewasa = +document.getElementById("cinaDewasaAhliBaru").value;
+  db.update(`${tahun}.${bulan}.${hari}.dewasa.ahliBaru.cina`, n => n + cinaDewasa)
+    .write()
+  document.getElementById("cinaDewasaAhliBaru").value = 1;
+  document.getElementById("cinaDewasaAhliBaruSemasa").value = db.get(`${tahun}.${bulan}.${hari}.dewasa.ahliBaru.cina`).value();
+
+  lastUpdate(`Ahli Baru Cina Dewasa + ${cinaDewasa}`)
+}
+
+
+
+function updateIndiaDewasaPengguna() {
+  var indiaDewasa = +document.getElementById("indiaDewasaPengguna").value;
+  db.update(`${tahun}.${bulan}.${hari}.dewasa.pengguna.india`, n => n + indiaDewasa)
+    .write()
+  db.update(`${tahun}.${bulan}.${hari}.dewasa.pengunjung.india`, n => n + indiaDewasa)
+    .write()
+  document.getElementById("indiaDewasaPengguna").value = 1;
+  document.getElementById("indiaDewasaPenggunaSemasa").value = db.get(`${tahun}.${bulan}.${hari}.dewasa.pengguna.india`).value();
+  document.getElementById("indiaDewasaPengunjungSemasa").value = db.get(`${tahun}.${bulan}.${hari}.dewasa.pengunjung.india`).value();
+
+  lastUpdate(`Pengguna India Dewasa + ${indiaDewasa}`)
+}
+function updateIndiaDewasaPengunjung() {
+  var indiaDewasa = +document.getElementById("indiaDewasaPengunjung").value;
+  db.update(`${tahun}.${bulan}.${hari}.dewasa.pengunjung.india`, n => n + indiaDewasa)
+    .write()
+  document.getElementById("indiaDewasaPengunjung").value = 1;
+  document.getElementById("indiaDewasaPengunjungSemasa").value = db.get(`${tahun}.${bulan}.${hari}.dewasa.pengunjung.india`).value();
+  lastUpdate(`Pengunjung India Dewasa + ${indiaDewasa}`)
+}
+function updateIndiaDewasaAhliBaru() {
+  var indiaDewasa = +document.getElementById("indiaDewasaAhliBaru").value;
+  db.update(`${tahun}.${bulan}.${hari}.dewasa.ahliBaru.india`, n => n + indiaDewasa)
+    .write()
+  document.getElementById("indiaDewasaAhliBaru").value = 1;
+  document.getElementById("indiaDewasaAhliBaruSemasa").value = db.get(`${tahun}.${bulan}.${hari}.dewasa.ahliBaru.india`).value();
+
+  lastUpdate(`Ahli Baru India Dewasa + ${indiaDewasa}`)
+}
+
+
+function updateLainLainDewasaPengguna() {
+  var lainDewasa = +document.getElementById("lainlainDewasaPengguna").value;
+  db.update(`${tahun}.${bulan}.${hari}.dewasa.pengguna.lainlain`, n => n + lainDewasa)
+    .write()
+  db.update(`${tahun}.${bulan}.${hari}.dewasa.pengunjung.lainlain`, n => n + lainDewasa)
+    .write()
+  document.getElementById("lainlainDewasaPengguna").value = 1;
+  document.getElementById("lainlainDewasaPenggunaSemasa").value = db.get(`${tahun}.${bulan}.${hari}.dewasa.pengguna.lainlain`).value();
+  document.getElementById("lainlainDewasaPengunjungSemasa").value = db.get(`${tahun}.${bulan}.${hari}.dewasa.pengunjung.lainlain`).value();
+
+  lastUpdate(`Pengguna Lain-lain Dewasa + ${lainDewasa}`)
+}
+function updateLainLainDewasaPengunjung() {
+  var lainDewasa = +document.getElementById("lainlainDewasaPengunjung").value;
+  db.update(`${tahun}.${bulan}.${hari}.dewasa.pengunjung.lainlain`, n => n + lainDewasa)
+    .write()
+  document.getElementById("lainlainDewasaPengunjung").value = 1;
+  document.getElementById("lainlainDewasaPengunjungSemasa").value = db.get(`${tahun}.${bulan}.${hari}.dewasa.pengunjung.lainlain`).value();
+
+  lastUpdate(`Pengunjung Lain-lain Dewasa + ${lainDewasa}`)
+}
+function updateLainLainDewasaAhliBaru() {
+  var lainDewasa = +document.getElementById("indiaDewasaAhliBaru").value;
+  db.update(`${tahun}.${bulan}.${hari}.dewasa.ahliBaru.lainlain`, n => n + lainDewasa)
+    .write()
+  document.getElementById("lainlainDewasaAhliBaru").value = 1;
+  document.getElementById("lainlainDewasaAhliBaruSemasa").value = db.get(`${tahun}.${bulan}.${hari}.dewasa.ahliBaru.lainlain`).value();
+
+  lastUpdate(`Ahli Baru Lain-lain Dewasa + ${lainDewasa}`)
+}
+
+
+function lastUpdate(value) {
+  var masa = new Date();
+  var jam = masa.getHours();
+  var minit = masa.getMinutes();
+  if (minit < 10) {
+    minit = '0' + minit;
+  }
+
+
+  masaSekarang = jam + ':' + minit;
+
+  document.getElementById("lastUpdate3").value = document.getElementById("lastUpdate2").value;
+  document.getElementById("lastUpdate2").value = document.getElementById("lastUpdate1").value;
+  document.getElementById("lastUpdate1").value = masaSekarang + ' ' + value;
+}
---
+++ report.js
@@ -0,0 +1,190 @@
+var Excel = require('exceljs');
+var workbook = new Excel.Workbook();
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+const adapter = new FileSync('db.json')
+const db = low(adapter)
+
+var tahun = 2018;
+var bulan = 10;
+var hari = 1;
+var bulanLetter;
+switch (bulan) {
+    case 1:
+        bulanLetter = "JANUARI";
+        break;
+    case 2:
+        bulanLetter = "FEBRUARI";
+        break;
+    case 3:
+        bulanLetter = "MAC";
+        break;
+    case 4:
+        bulanLetter = "APRIL";
+        break;
+    case 5:
+        bulanLetter = "MEI";
+        break;
+    case 6:
+        bulanLetter = "JUN";
+        break;
+    case 7:
+        bulanLetter = "JULAI";
+        break;
+    case 8:
+        bulanLetter = "OGOS";
+        break;
+    case 9:
+        bulanLetter = "SEPTEMBER";
+        break;
+    case 10:
+        bulanLetter = "OKTOBER";
+        break;
+    case 11:
+        bulanLetter = "NOVEMBER";
+        break;
+    case 12:
+        bulanLetter = "DISEMBER";
+        break;
+}
+
+
+
+function cetakExcel(){
+workbook.xlsx.readFile('formatOriginalJANGANEDIT.xlsx')
+    .then(function () {
+
+        var i;
+        var worksheet = workbook.getWorksheet(1);
+
+        worksheet.getRow(10).getCell(1).value = `STATISTIK PENGGUNA, PENGUNJUNG DAN KEAHLIAN PERPUSTAKAAN BULAN ${bulanLetter} TAHUN ${tahun}`;
+
+        worksheet.getRow(27).getCell(1).value = `JUMLAH PENGGUNA (${bulanLetter} ${tahun})`;
+        worksheet.getRow(27).getCell(11).value = `JUMLAH PENGUNJUNG (${bulanLetter} ${tahun})`;
+        worksheet.getRow(27).getCell(24).value = `JUMLAH AHLI BARU (${bulanLetter} ${tahun})`;
+
+
+        worksheet.getRow(51).getCell(1).value = `STATISTIK KEAHLIAN & PENGGUNA MENGIKUT KAUM BAGI BULAN ${bulanLetter} TAHUN ${tahun}`;
+        
+        worksheet.getRow(68).getCell(1).value = `JUMLAH KEAHLIAN MENGIKUT KAUM (${bulanLetter} ${tahun})`;
+        worksheet.getRow(68).getCell(18).value = `JUMLAH PENGGUNA MENGIKUT KAUM (${bulanLetter} ${tahun})`;
+        
+        
+        worksheet.getRow(91).getCell(1).value = `STATISTIK PENGUNJUNG MENGIKUT KAUM BAGI BULAN ${bulanLetter} TAHUN ${tahun}`;
+
+        worksheet.getRow(101).getCell(1).value = `JUMLAH PENGUNJUNG MENGIKUT KAUM (${bulanLetter} ${tahun})`;
+        
+        
+        
+        
+        
+        
+        for (i = 0; i < 31; i++) {
+
+            worksheet.getRow(15).getCell(i + 2).value =
+                db.get(`${tahun}.${bulan}.${hari + i}.belia.pengguna.melayu`).value()
+                + db.get(`${tahun}.${bulan}.${hari + i}.belia.pengguna.cina`).value()
+                + db.get(`${tahun}.${bulan}.${hari + i}.belia.pengguna.india`).value()
+                + db.get(`${tahun}.${bulan}.${hari + i}.belia.pengguna.lainlain`).value()
+                ;
+
+            worksheet.getRow(19).getCell(i + 2).value =
+                db.get(`${tahun}.${bulan}.${hari + i}.belia.pengunjung.melayu`).value()
+                + db.get(`${tahun}.${bulan}.${hari + i}.belia.pengunjung.cina`).value()
+                + db.get(`${tahun}.${bulan}.${hari + i}.belia.pengunjung.india`).value()
+                + db.get(`${tahun}.${bulan}.${hari + i}.belia.pengunjung.lainlain`).value()
+                ;
+
+            worksheet.getRow(23).getCell(i + 2).value =
+                db.get(`${tahun}.${bulan}.${hari + i}.belia.ahliBaru.melayu`).value()
+                + db.get(`${tahun}.${bulan}.${hari + i}.belia.ahliBaru.cina`).value()
+                + db.get(`${tahun}.${bulan}.${hari + i}.belia.ahliBaru.india`).value()
+                + db.get(`${tahun}.${bulan}.${hari + i}.belia.ahliBaru.lainlain`).value()
+                ;
+
+            worksheet.getRow(16).getCell(i + 2).value =
+                db.get(`${tahun}.${bulan}.${hari + i}.dewasa.pengguna.melayu`).value()
+                + db.get(`${tahun}.${bulan}.${hari + i}.dewasa.pengguna.cina`).value()
+                + db.get(`${tahun}.${bulan}.${hari + i}.dewasa.pengguna.india`).value()
+                + db.get(`${tahun}.${bulan}.${hari + i}.dewasa.pengguna.lainlain`).value()
+                ;
+
+            worksheet.getRow(20).getCell(i + 2).value =
+                db.get(`${tahun}.${bulan}.${hari + i}.dewasa.pengunjung.melayu`).value()
+                + db.get(`${tahun}.${bulan}.${hari + i}.dewasa.pengunjung.cina`).value()
+                + db.get(`${tahun}.${bulan}.${hari + i}.dewasa.pengunjung.india`).value()
+                + db.get(`${tahun}.${bulan}.${hari + i}.dewasa.pengunjung.lainlain`).value()
+                ;
+
+            worksheet.getRow(24).getCell(i + 2).value =
+                db.get(`${tahun}.${bulan}.${hari + i}.dewasa.ahliBaru.melayu`).value()
+                + db.get(`${tahun}.${bulan}.${hari + i}.dewasa.ahliBaru.cina`).value()
+                + db.get(`${tahun}.${bulan}.${hari + i}.dewasa.ahliBaru.india`).value()
+                + db.get(`${tahun}.${bulan}.${hari + i}.dewasa.ahliBaru.lainlain`).value()
+                ;
+
+            worksheet.getRow(56).getCell(i + 2).value =
+                db.get(`${tahun}.${bulan}.${hari + i}.dewasa.ahliBaru.melayu`).value()
+                + db.get(`${tahun}.${bulan}.${hari + i}.belia.ahliBaru.melayu`).value();
+
+            worksheet.getRow(57).getCell(i + 2).value =
+                db.get(`${tahun}.${bulan}.${hari + i}.dewasa.ahliBaru.cina`).value()
+                + db.get(`${tahun}.${bulan}.${hari + i}.belia.ahliBaru.cina`).value();
+
+            worksheet.getRow(58).getCell(i + 2).value =
+                db.get(`${tahun}.${bulan}.${hari + i}.dewasa.ahliBaru.india`).value()
+                + db.get(`${tahun}.${bulan}.${hari + i}.belia.ahliBaru.india`).value();
+
+            worksheet.getRow(59).getCell(i + 2).value =
+                db.get(`${tahun}.${bulan}.${hari + i}.dewasa.ahliBaru.lainlain`).value()
+                + db.get(`${tahun}.${bulan}.${hari + i}.belia.ahliBaru.lainlain`).value();
+
+
+            worksheet.getRow(62).getCell(i + 2).value =
+                db.get(`${tahun}.${bulan}.${hari + i}.dewasa.pengguna.melayu`).value()
+                + db.get(`${tahun}.${bulan}.${hari + i}.belia.pengguna.melayu`).value();
+
+            worksheet.getRow(63).getCell(i + 2).value =
+                db.get(`${tahun}.${bulan}.${hari + i}.dewasa.pengguna.cina`).value()
+                + db.get(`${tahun}.${bulan}.${hari + i}.belia.pengguna.cina`).value();
+
+            worksheet.getRow(64).getCell(i + 2).value =
+                db.get(`${tahun}.${bulan}.${hari + i}.dewasa.pengguna.india`).value()
+                + db.get(`${tahun}.${bulan}.${hari + i}.belia.pengguna.india`).value();
+
+            worksheet.getRow(65).getCell(i + 2).value =
+                db.get(`${tahun}.${bulan}.${hari + i}.dewasa.pengguna.lainlain`).value()
+                + db.get(`${tahun}.${bulan}.${hari + i}.belia.pengguna.lainlain`).value();
+
+
+
+
+
+            worksheet.getRow(95).getCell(i + 2).value =
+                db.get(`${tahun}.${bulan}.${hari + i}.dewasa.pengunjung.melayu`).value()
+                + db.get(`${tahun}.${bulan}.${hari + i}.belia.pengunjung.melayu`).value();
+
+            worksheet.getRow(96).getCell(i + 2).value =
+                db.get(`${tahun}.${bulan}.${hari + i}.dewasa.pengunjung.cina`).value()
+                + db.get(`${tahun}.${bulan}.${hari + i}.belia.pengunjung.cina`).value();
+
+            worksheet.getRow(97).getCell(i + 2).value =
+                db.get(`${tahun}.${bulan}.${hari + i}.dewasa.pengunjung.india`).value()
+                + db.get(`${tahun}.${bulan}.${hari + i}.belia.pengunjung.india`).value();
+
+            worksheet.getRow(98).getCell(i + 2).value =
+                db.get(`${tahun}.${bulan}.${hari + i}.dewasa.pengunjung.lainlain`).value()
+                + db.get(`${tahun}.${bulan}.${hari + i}.belia.pengunjung.lainlain`).value();
+
+
+        }
+        worksheet.getRow().commit();
+        return workbook.xlsx.writeFile(`statistik bulan ${bulan} ${tahun}.xlsx`);
+
+
+
+
+
+    })
+
+}
