f9a9b57536440bf8183fe6be5a6ccd667e9f3d78
kamil zobri <kamilzobri@gmail.com>
2018-11-08 11:03:44 +0800

    working fix bug dan lain

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 600, height: 700,resizable:false,maximizable:false, alwaysOnTop:true})
+  mainWindow = new BrowserWindow({width: 600, height: 400,resizable:false,maximizable:false, alwaysOnTop:true})
   
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
@@ -53,8 +53,8 @@ function createWindow () {
       role: 'help',
       submenu: [
         {
-          label: 'Learn More',
-          click () { require('electron').shell.openExternal('https://electronjs.org') }
+          label: 'Hasilkan Laporan',
+          click () { require('./laporan.js').cetakExcel() }
         }
       ]
     }
